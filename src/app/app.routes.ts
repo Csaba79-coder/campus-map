767000a25bcd6c204186a050586d6231bdfff385
Csaba Vadász <csabavadasz79@gmail.com>
2026-05-04 15:17:56 +0200

    Add Home component and configure routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/guards/auth-guard';
+import { guestGuard } from './auth/guards/guest-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () =>
+      import('./auth/login/login').then((m) => m.Login),
+  },
+  {
+    path: '',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./home/home').then((m) => m.Home),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
