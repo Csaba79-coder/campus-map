2eb80aadbdc8901363deae46a202125cd53a4a42
Csaba Vadász <csabavadasz79@gmail.com>
2026-05-05 18:55:54 +0200

    Add bidirectional map-list synchronization on Home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -18,7 +18,10 @@ export class Home implements OnInit, AfterViewInit {
 
   buildings = signal<Building[]>([]);
   loading = signal(false);
+  selectedBuildingId = signal<number | null>(null);
+
   private map!: L.Map;
+  private polygonMap = new Map<number, L.Polygon>();
 
   ngOnInit(): void {
     this.loadBuildings();
@@ -60,13 +63,13 @@ export class Home implements OnInit, AfterViewInit {
         const latLngs = building.polygon.map((c) => L.latLng(c[0], c[1]));
         const poly = L.polygon(latLngs, { color: '#3388ff' }).addTo(this.map);
 
-        poly.bindTooltip(`
-            <strong>${building.name}</strong>
-            <br>${building.description}<br>
-            <em>Points: ${building.polygon.length}</em>`,
-          { sticky: true }
-        );
+        poly.bindTooltip(`<strong>${building.name}</strong><br>${building.description}<br><em>Points: ${building.polygon.length}</em>`, { sticky: true });
 
+        poly.on('click', () => {
+          this.selectBuilding(building.id!);
+        });
+
+        this.polygonMap.set(building.id!, poly);
         group.addLayer(poly);
       }
     });
@@ -74,5 +77,20 @@ export class Home implements OnInit, AfterViewInit {
     if (group.getLayers().length > 0) {
       this.map.fitBounds(group.getBounds(), { padding: [20, 20] });
     }
+  }
+
+  selectFromList(building: Building): void {
+    this.selectBuilding(building.id!);
+    const poly = this.polygonMap.get(building.id!);
+    if (poly) {
+      this.map.fitBounds(poly.getBounds(), { padding: [50, 50] });
+    }
+  }
+
+  private selectBuilding(id: number): void {
+    this.polygonMap.forEach((poly, polyId) => {
+      poly.setStyle({ color: polyId === id ? '#ff4444' : '#3388ff', weight: polyId === id ? 4 : 3 });
+    });
+    this.selectedBuildingId.set(id);
   }
 }
